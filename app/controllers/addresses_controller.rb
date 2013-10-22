903589e8b5a0b82843a1e5b70bc93364bfe01e09
Raghu Betina <rbetina@gmail.com>
2013-10-22 16:13:27 -0500

    Disabled SSL and added more hints

--- app/controllers/addresses_controller.rb
+++ app/controllers/addresses_controller.rb
@@ -4,10 +4,12 @@ require 'json'
 class AddressesController < ApplicationController
   def fetch_coordinates
     @address = "the corner of Foster and Sheridan"
-    url_safe_address = URI.encode(@address)
+    @url_safe_address = URI.encode(@address)
 
     # Your code goes here.
-
+    # url = ?
+    # raw_data = ?
+    # parsed_data = ?
     # @latitude = ?
     # @longitude = ?
   end
