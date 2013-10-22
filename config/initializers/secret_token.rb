6f8b07710924cfa994c1aba7ed3fdb2e3ab6dab2
Raghu Betina <rbetina@gmail.com>
2013-10-22 13:58:45 -0500

    rails new

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+AddressToCoords::Application.config.secret_key_base = 'e86932f9083996f1ebdfd2b0cc32165b6a05ad0187f51c6d2c65ba4d46bbcf2724fec923e2ce37821d82b43a8640f174dde63c04896738f41252a5600464009f'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+AddressToCoords::Application.config.session_store :cookie_store, key: '_address_to_coords_session'
